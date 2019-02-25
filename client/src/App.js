7257e4243d3c5be7cfc52405b5df0043a62a61ec
James Basile <jbasile14@gmail.com>
2019-02-25 17:47:46 -0500

    click on movie card redirect to movie details complete

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route path='/' component={ MovieList }/>
-        <Route path='movies/:id' />
+        <Route path='/' exact component={ MovieList }/>
+        <Route path='/movies/:id' component={ Movie }/>
       </div>
     );
   }
